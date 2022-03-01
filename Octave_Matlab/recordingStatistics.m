073b14516faae6cf5db2b247fac05824016f12ca
Markus Ebner <hiwatari.seiji@gmail.com>
2022-03-01 11:06:49 +0100

    Add more analysis plots to recordingStatistics

--- Octave_Matlab/recordingStatistics.m
+++ Octave_Matlab/recordingStatistics.m
@@ -1,6 +1,7 @@
 clear all;
 close all;
 loadSensorReadoutParser('src/');
+GRID_WIDTH = 4;
 
 % #########################################################################
 % # Select File
@@ -52,3 +53,42 @@ wifiPerSec = length(wifiTimestamps) / (max(wifiTimestamps) - min(wifiTimestamps)
 printf('\tWifi events: %f/s\n', wifiPerSec);
 ftmPerSec = length(ftmTimestamps) / (max(ftmTimestamps) - min(ftmTimestamps));
 printf('\tFTM events: %f/s\n', ftmPerSec);
+
+fprintf('=== FTM Statistics ===\n');
+printf('\tAvg. distance: %.2f mm\n', mean([ftmMeasurements{:,4}]));
+printf('\tStddev. distance: %.2f mm\n', std([ftmMeasurements{:,4}]));
+figure('name', 'Measured FTM Distance distribution');
+hist([ftmMeasurements{:,4}]);
+
+figure('name', 'Measured FTM Distance distributions by device');
+hold on;
+rangingMacs = unique(ftmMeasurements(:,3));
+minFtmDist = min([ftmMeasurements{:,4}]);
+maxFtmDist = max([ftmMeasurements{:,4}]);
+
+gridHeight = ceil(rows(rangingMacs) / GRID_WIDTH);
+for(rangingId = [1:length(rangingMacs)])
+	rangingMac = rangingMacs{rangingId};
+	macIdcs = strcmp(rangingMac, ftmMeasurements(:,3));
+	subplot(gridHeight, GRID_WIDTH, rangingId);
+	hist([ftmMeasurements{macIdcs,4}]);
+	xlim([minFtmDist, maxFtmDist]);
+	title(rangingMac);
+end
+hold off;
+
+figure('name', 'FTM measurement count per device');
+ftmMeasurementCnts = [];
+for(rangingId = [1:length(rangingMacs)])
+	rangingMac = rangingMacs{rangingId};
+	macIdcs = strcmp(rangingMac, ftmMeasurements(:,3));
+	measurementCnt = sum(macIdcs);
+	ftmMeasurementCnts = [ftmMeasurementCnts, measurementCnt];
+end
+hold on;
+barh([1:length(rangingMacs)], ftmMeasurementCnts);
+for(rangingId = [1:length(rangingMacs)])
+	text(0.05 * max(ftmMeasurementCnts), rangingId, rangingMacs{rangingId});
+end
+hold off;
+
